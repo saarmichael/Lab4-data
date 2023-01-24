697df558ecc7e6680c06a916d127173b664268ce
aviadbensh <aviadbensh2@gmail.com>
2023-01-24 23:51:52 +0200

    added firing rate function

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 from plot_signal import plot_signal
 from spike_detection import spike_detection
 from plot_spikes import plot_spikes
+from Firing_rate import firingRate
 
 # import the data from .mat files
 import scipy.io as sio
+
+# raw signal
 raw_still           = sio.loadmat('CWB_Still.mat')['StillWB'][0]
 raw_still_freq      = sio.loadmat('CWB_Still.mat')['sampling_freq'][0][0]
 raw_move            = sio.loadmat('CWB_Move.mat')['MoveWB'][0]
 raw_move_freq       = sio.loadmat('CWB_Move.mat')['sampling_freq'][0][0]
+
+# lfp signal
 lfp_still           = sio.loadmat('CLFP_Still.mat')['LFPStill'][0]
 lfp_still_freq      = sio.loadmat('CLFP_Still.mat')['sampling_freq'][0][0]
 lfp_move            = sio.loadmat('CLFP_Move.mat')['LFPMove'][0]
 lfp_move_freq       = sio.loadmat('CLFP_Move.mat')['sampling_freq'][0][0]
+
+# spike signal
 spk_still           = sio.loadmat('CSPK_Still.mat')['StillSPK'][0]
 spk_still_freq      = sio.loadmat('CSPK_Still.mat')['sampling_freq'][0][0]
 spk_move            = sio.loadmat('CSPK_Move.mat')['MoveSPK'][0]
@@ -28,4 +35,9 @@ condition = 'Move'
 
 # detect spikes in the still signals
 detected = spike_detection(spk_move[:int(spk_move_freq/divider)], spk_move_freq, False)
-plot_spikes( spk_move[:int(spk_move_freq/divider)], detected, freq = spk_move_freq)
+print(detected)
+# plot_spikes( spk_move[:int(spk_move_freq/divider)], detected, freq = spk_move_freq)
+
+# calculate the firing rate of the neuron
+firing_rate = firingRate(detected, spk_move_freq, len(spk_move[:int(spk_move_freq/divider)])/spk_move_freq)
+print(firing_rate)
